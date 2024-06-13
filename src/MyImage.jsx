e9ea83846075dcecd91df0bf74e41cbf561d556a
Apoorv Mittal <apoorvmitthu@gmail.com>
2024-06-13 15:07:45 +0530

    display image in center, and not overflow it

--- src/MyImage.jsx
+++ src/MyImage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import "./MyImage.css"
 
 function MyImage() {
   const {code} = useParams();
@@ -21,14 +22,13 @@ function MyImage() {
   );
   
   return (
-    // <p>MyImage: {code}</p>
     <>
       {error && <p>{error}</p>}
       {!error &&
-      <>
-        {url && <img src={url} alt="image" />}
+      <div className='image-container'>
+        {url && <img src={url} className='my-image' alt="image" />}
         {!url && <p>Loading...</p>}
-      </>}
+      </div>}
     </>
 
     // <img src="https://images.unsplash.com/photo-1587474260584-136574528ed5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVsaGl8ZW58MHx8MHx8fDA%3D" alt="image" />
